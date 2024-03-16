8eddbf4ebb589833763bb730c304a7cac2bc9cc5
adrimartiin <466162.joan23@fje.edu>
2024-03-16 01:01:22 +0100

    index

---
+++ index.php
@@ -0,0 +1,31 @@
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+    <link rel="stylesheet" type="text/css" href="./css/styles.css">
+</head>
+<body id="index">
+    <div class="row">
+        <div class="col1">
+            <img class="img" src="./img/img2.jpg" alt="WorldCup">
+        </div>
+        <div class="col2">
+            
+                <h1 class="titulo"> Football Escape Room </h1>
+            
+                <p class="parraf"> Bienvenido al juego más enigmático del mundo del fútbol</p>
+
+                <form action="brasil.php" method="post">
+                    <button class="bot2" type="submit">Jugar</button>
+                </form>
+                <form action="normas.php" method="post">
+                    <button class="bot" type="submit">Normas</button>
+                </form>
+            
+        </div>
+        
+    </div>
+</body>
+</html>
